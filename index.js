16b97589e783827329a89ddcc79226f5164fdc3a
Bashua Mutiat <mutiatbashua@gmail.com>
2023-02-13 01:24:57 +0100

    added javascript

---
+++ index.js
@@ -0,0 +1,35 @@
+// function that multiplies two numbers
+
+function multiply(a, b) {
+  return a + b;
+}
+multiply(4, 3);
+console.log(multiply(4, 3));
+
+// creating variables
+const firstName = "Mutiat";
+const lastName = "Bashua";
+let track = "front-end development";
+let skills = ["HTML", "CSS", "JavaScript"];
+let favColor = "pink";
+const independence = 1960;
+const sentence =
+  "My name is " +
+  firstName +
+  " " +
+  lastName +
+  "," +
+  "a student of Stutern accelerator program in " +
+  track +
+  " track." +
+  " My skills are " +
+  skills +
+  " and my favourite color is " +
+  favColor +
+  "." +
+  " I am from Nigeria and my country gained independence in " +
+  independence +
+  "." +
+  " I look forward to learning more about Javascript";
+("");
+console.log(sentence);
